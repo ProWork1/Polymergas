828123fde1b99e6e68511743420df5e70cbaf754
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2021-07-29 16:28:24 +0500

    started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import MainCarousel from './Components/MainCarousel/MainCarousel'
+
+function App() {
+    return (
+        <div className='App'>
+            <div>
+                <MainCarousel />
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/MainCarousel/MainCarousel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './mainCarousel.scss'
+
+const MainCarousel = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default MainCarousel
