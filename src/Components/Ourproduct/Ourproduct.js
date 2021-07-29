4ba5711cef9ce9dd43fc0c9249ebdaa587962051
MurodkhonJalolov <murodkhanjalolov2004@gmail.com>
2021-07-29 19:25:25 +0500

    ourproduct

---
+++ src/Components/Ourproduct/Ourproduct.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ourproduct.scss'
+
+const Ourproduct = () => {
+    return (
+        <div>
+            <section className="container">
+                Bizning mahsulotlar
+                Bizdagi mavjud mahsulotlarni o'zingizga ma'qul narxlarda harid qiling
+            </section>
+            
+        </div>
+    )
+}
+
+export default Ourproduct
