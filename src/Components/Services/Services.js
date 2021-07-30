49642e439b021817b0cd7d31417e6451f1c080b0
Musojon-T <musojontursunov1@gmail.com>
2021-07-30 12:30:40 +0500

    Services

---
+++ src/Components/Services/Services.js
@@ -0,0 +1,215 @@
+import React from "react";
+import "./Services.scss";
+function Services() {
+  return (
+    <div id={"Services"} className={"Services"}>
+      <div className={"container"}>
+        <div className={"title"}>
+          <h1>Bizning mahsulotlar sizni biznesingizni rivojlantirish uchun </h1>
+          <p>Biz bilan hamkorlik qiling va sifatli hom-ashyoga ega bo'ling</p>
+        </div>
+        <div className={"row pt-5"}>
+          <div className={"col-md-4"}>
+            <div className={"card-body"}>
+              <div className={"svg"}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="none"
+                >
+                  <g clip-path="url(#a)">
+                    <circle cx="22" cy="22" r="22" fill="#00aa8d3d"></circle>
+                    <path
+                      stroke="#00AA8D"
+                      stroke-width="2"
+                      d="M36 16l10 10M32 19.06c-4.65-2.05-10.25-1.13-14.04 2.76-.78.8-1.43 1.67-1.96 2.6L21.45 30M42.9 30c2.1 4.94 1.2 10.9-2.7 14.93-.8.83-1.68 1.5-2.6 2.07L32 41.2"
+                    ></path>
+                    <path
+                      fill-rule="evenodd"
+                      stroke="#00AA8D"
+                      stroke-width="2"
+                      d="M28.23 43L19 33.77S26.88 14.15 49 13c-1.25 22.02-20.77 30-20.77 30zM13 45c0-2.2 1.8-4 4-4s4 1.8 4 4-1.8 4-4 4c-.9 0-4 0-4 0s0-2.7 0-4zM33.5 32c1.93 0 3.5-1.57 3.5-3.5S35.43 25 33.5 25 30 26.57 30 28.5s1.57 3.5 3.5 3.5z"
+                      clip-rule="evenodd"
+                      stroke-linecap="square"
+                    ></path>
+                  </g>
+                  <defs>
+                    <clipPath id="a">
+                      <path fill="#fff" d="M0 0h50v50H0z"></path>
+                    </clipPath>
+                  </defs>
+                </svg>
+              </div>
+              <h4>Tez Start Oling</h4>
+              <p>Ishlab chiqarish uchun eng ma'qul narx va sifat</p>
+            </div>
+          </div>
+          <div className={"col-md-4 "}>
+            <div className={"card-body"}>
+              <div className={"svg"}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="none"
+                >
+                  <circle cx="22" cy="22" r="22" fill="#00aa8d3d"></circle>
+                  <path
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M25.5 21.5h2M32.5 21.5h10M25.5 27.5h2M32.5 27.5h10M25.5 34.5h2M32.5 34.5h10M25.5 40.5h8"
+                    stroke-linecap="square"
+                  ></path>
+                  <path
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M38.5 49V39h10"
+                  ></path>
+                  <path
+                    fill-rule="evenodd"
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M38.83 49H19.5V13h29v26.18L38.83 49z"
+                    clip-rule="evenodd"
+                    stroke-linecap="square"
+                  ></path>
+                </svg>
+              </div>
+              <h4>Keng Assortiment</h4>
+              <p>Turli mahsulotlar uchun polimerlarning keng turlari</p>
+            </div>
+          </div>
+          <div className={"col-md-4"}>
+            <div className={"card-body"}>
+              <div className={"svg"}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="none"
+                >
+                  <circle cx="22" cy="22" r="22" fill="#00aa8d3d"></circle>
+                  <path
+                    fill-rule="evenodd"
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M47.5 15.5h-18v16l6.75-5.33H47.5V15.5zM29.5 46.75c0-.9-.4-1.76-1.15-2.33-1.12-.86-3.07-1.92-5.85-1.92s-4.73 1.06-5.85 1.92c-.74.57-1.15 1.43-1.15 2.33v1.75h14v-1.75zM22 38.5c1.93 0 3.5-1.57 3.5-3.5s-1.57-3.5-3.5-3.5-3.5 1.57-3.5 3.5 1.57 3.5 3.5 3.5zM48.5 46.75c0-.9-.4-1.76-1.15-2.33-1.12-.86-3.07-1.92-5.85-1.92s-4.73 1.06-5.85 1.92c-.74.57-1.15 1.43-1.15 2.33v1.75h14v-1.75zM42 38.5c1.93 0 3.5-1.57 3.5-3.5s-1.57-3.5-3.5-3.5-3.5 1.57-3.5 3.5 1.57 3.5 3.5 3.5z"
+                    clip-rule="evenodd"
+                    stroke-linecap="square"
+                  ></path>
+                </svg>
+              </div>
+              <h4>Ko'pchilik Tanlovi</h4>
+              <p>Haridorlarimizga ma'qul sifat va tanlov</p>
+            </div>
+          </div>
+          <div className={"col-md-4"}>
+            <div className={"card-body"}>
+              <div className={"svg"}>
+                <svg
+                  height="50"
+                  viewBox="0 0 64 64"
+                  width="50"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="line">
+                    <path
+                      d="m50.95 37a12.865 12.865 0 0 1 -9.157-3.793 10.879 10.879 0 0 0 -7.743-3.207h-21.05v22a3 3 0 0 0 3 3h44a3 3 0 0 0 3-3v-15zm-12.95 12h-19a1 1 0 0 1 0-2h19a1 1 0 0 1 0 2z"
+                      fill="#00aa8d3d"
+                    ></path>
+                    <path
+                      d="m34.05 28a12.865 12.865 0 0 1 9.157 3.793 10.879 10.879 0 0 0 7.743 3.207h12.05v-11a3 3 0 0 0 -3-3h-44a3 3 0 0 0 -3 3v4z"
+                      fill="#00aa8d3d"
+                    ></path>
+                    <g fill="#00AA8D">
+                      <path d="m48.044 9h-44a3 3 0 0 0 -3 3c0 10.764-.1 10 0 10v18a3 3 0 0 0 3 3h44a3 3 0 0 0 3-3v-28a3 3 0 0 0 -3-3zm-44 2h44a1 1 0 0 1 1 1v3h-46v-3a1 1 0 0 1 1-1zm45 10h-46v-4h46zm-1 20h-44a1 1 0 0 1 -1-1v-17h46v17a1 1 0 0 1 -1 1z"></path>
+                      <path d="m28.044 33h-18a1 1 0 1 0 0 2h18a1 1 0 0 0 0-2z"></path>
+                      <path d="m40.044 30h-4a3 3 0 0 0 -3 3v2a3 3 0 0 0 3 3h4a3 3 0 0 0 3-3v-2a3 3 0 0 0 -3-3zm1 5a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1-1v-2a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1z"></path>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+              <h4>Qulay To'lov</h4>
+              <p>
+                Siz o'zingizga qulay bo'lgan to'lov turini tanlashingiz mumkin
+              </p>
+            </div>
+          </div>
+          <div className={"col-md-4"}>
+            <div className={"card-body"}>
+              <div className={"svg"}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="none"
+                >
+                  <circle cx="22" cy="22" r="22" fill="#00aa8d3d"></circle>
+                  <path
+                    fill-rule="evenodd"
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M45.2 28.1l-13.37-2.28V17.7c0-1.5-1.2-2.7-2.7-2.7-1.5 0-2.7 1.2-2.7 2.7v12.75H24.1c-1.7 0-3.1 1.4-3.1 3.1v4.47c0 2.1.72 4.15 2.04 5.8L26.42 49h20.12l2.4-15.63c.4-2.48-1.28-4.82-3.75-5.27z"
+                    clip-rule="evenodd"
+                    stroke-linecap="square"
+                  ></path>
+                  <path
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M34.5 35v8M40.5 35v8"
+                    stroke-linecap="square"
+                  ></path>
+                  <path fill="#00AA8D" d="M25.42 27.4h2v6.96h-2z"></path>
+                </svg>
+              </div>
+              <h4>Oson Xarid</h4>
+              <p>Mahsulotlarni telegram bot orqali xarid qiling</p>
+            </div>
+          </div>
+          <div className={"col-md-4"}>
+            <div className={"card-body"}>
+              <div className={"svg"}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="none"
+                >
+                  <circle cx="22" cy="22" r="22" fill="#00aa8d3d"></circle>
+                  <path
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M44.33 20H49v23H28M18.57 43H14V20h16"
+                    stroke-linecap="square"
+                  ></path>
+                  <path
+                    fill-rule="evenodd"
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M43.18 31H30.82c-.45 0-.82-.36-.82-.8V15.8c0-.44.37-.8.82-.8h12.36c.45 0 .82.36.82.8v14.4c0 .44-.37.8-.82.8zM27.18 49h-7.36c-.45 0-.82-.36-.82-.8V36.8c0-.44.37-.8.82-.8h7.36c.45 0 .82.36.82.8v11.4c0 .44-.37.8-.82.8z"
+                    clip-rule="evenodd"
+                    stroke-linecap="square"
+                  ></path>
+                  <path
+                    stroke="#00AA8D"
+                    stroke-width="2"
+                    d="M31 47.5h5M23 44.5h1M36 27.5h2"
+                    stroke-linecap="square"
+                  ></path>
+                </svg>
+              </div>
+              <h4>Tizimli Boshqaruv</h4>
+              <p>
+                Buyurtmalarni tizimli boshqarish va kamchiliklarga yo'l
+                qo'ymaslik
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
